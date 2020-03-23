178d4a0694de85cddbc14219a0c2570823825675
SwingbyDevJ <SwingbyDevJ@swingby.network>
2020-03-23 21:34:10 +0000

    setup.py: change swingby-python-sdk -> swingby

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="swingby-python-sdk", # Replace with your own username
+    name="swingby", # Replace with your own username
     version="1.0.0",
     author="Jacob Samuel",
     author_email="jacob@swingby.network",
